2cd07364e6c5078a3b76f33b5b7a5bf84c99ab6b
Maria Stashenko <mahryia@gmail.com>
2026-05-07 17:15:09 +0300

    add: users domain entity, value objects, exceptions

---
+++ src/vigil/modules/users/domain/exceptions.py
@@ -0,0 +1,30 @@
+from vigil.shared.exceptions import BusinessRuleException, BusinessRuleValidationException
+
+
+class UserNotFoundError(BusinessRuleException):
+	def __init__(self, message: str = "User not found"):
+		super().__init__(message)
+
+
+class UserAlreadyExistsError(BusinessRuleException):
+	def __init__(self, message: str = "User already exists"):
+		super().__init__(message)
+
+
+class InvalidCredentialsError(BusinessRuleException):
+	def __init__(self, message: str = "User not found"):
+		super().__init__(message)
+
+
+class EmailAlreadyTakenError(UserAlreadyExistsError):
+	def __init__(self, message: str = "User with this email already exists"):
+		super().__init__(message)
+
+
+class UsernameAlreadyTakenError(UserAlreadyExistsError):
+	def __init__(self, message: str = "User with this username already exists"):
+		super().__init__(message)
+
+
+class InvalidCredentialsFormatError(BusinessRuleValidationException):
+    pass
