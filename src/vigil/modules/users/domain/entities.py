c69af3a69fb44960809c9dd91adcf680b00e3e54
Maria Stashenko <mahryia@gmail.com>
2026-05-07 18:57:43 +0300

    fix: add username to domain user

--- src/vigil/modules/users/domain/entities.py
+++ src/vigil/modules/users/domain/entities.py
@@ -1,10 +1,11 @@
 from vigil.shared.base import Entity
-from .value_objects import UserId, Email
+from .value_objects import UserId, Email, Username
 
 
 class User(Entity):
 
-	def __init__(self, id: UserId, email: Email, password_hash: str):
+	def __init__(self, id: UserId, email: Email, username: Username, password_hash: str):
 		self.id: UserId = id
 		self.email: Email = email
+		self.username: Username = username
 		self.password_hash: str = password_hash
