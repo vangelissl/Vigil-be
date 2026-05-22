ae4df12a753eb641eea7746f53175ec14a7ecbe0
Maria Stashenko <mahryia@gmail.com>
2026-05-22 16:50:37 +0300

    add video db model and update user model

--- src/vigil/core/database/models/user.py
+++ src/vigil/core/database/models/user.py
@@ -1,18 +1,27 @@
+from typing import List
+
 from datetime import UTC, datetime
 
 import uuid
 
 from sqlalchemy import DateTime, String, UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..base import Base
 
 
 class UserModel(Base):
-	__tablename__ = 'users'
+    __tablename__ = 'users'
 
-	id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, index=True)
-	email: Mapped[str] = mapped_column(String(256), unique=True, nullable=False, index=True)
-	username: Mapped[str] = mapped_column(String(256), unique=True, nullable=False, index=True)
-	password_hash: Mapped[str] = mapped_column(String, nullable=False)
-	created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(UTC))
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(
+        String(256), unique=True, nullable=False, index=True)
+    username: Mapped[str] = mapped_column(
+        String(256), unique=True, nullable=False, index=True)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(UTC))
+
+    videos: Mapped[List["VideoModel"]] = relationship( # type: ignore
+        back_populates="owner")  
