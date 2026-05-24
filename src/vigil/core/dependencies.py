fbbfbd0c1004e970726990c2ab6a72975ca6d49c
Maria Stashenko <mahryia@gmail.com>
2026-05-24 19:41:48 +0300

    add minio storage wrapper

--- src/vigil/core/dependencies.py
+++ src/vigil/core/dependencies.py
@@ -1,6 +1,23 @@
+from fastapi import Depends
+
+from minio import Minio
+
 from redis.asyncio import Redis
 from .redis.pool import pool
+
+from .minio.storage import MinioStorage
+
+from .config import settings
 
 
 async def get_redis() -> Redis:
 	return Redis(connection_pool=pool)
+
+async def get_minio_storage() -> MinioStorage:
+	client = Minio(
+		endpoint=settings.minio_endpoint,
+		access_key=settings.minio_access_key,
+		secret_key=settings.minio_secret_key,
+		secure=settings.minio_secure
+	)
+	return MinioStorage(client, settings.minio_bucket)
