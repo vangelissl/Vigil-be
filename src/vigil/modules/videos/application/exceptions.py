09ba126cace37e4d0d88254c6675796614447e4d
Maria Stashenko <mahryia@gmail.com>
2026-05-24 22:34:05 +0300

    add videos endpoints

--- src/vigil/modules/videos/application/exceptions.py
+++ src/vigil/modules/videos/application/exceptions.py
@@ -8,3 +8,7 @@ class FilenameNoneOrEmptyError(InvalidFileError):
 class SizeUnknownError(InvalidFileError):
 	def __init__(self, message: str = "Video size is unknown"):
 		super().__init__(message)
+
+class FileAccessDeniedError(Exception):
+	def __init__(self, message: str = "Access to file denied"):
+		super().__init__(message)
