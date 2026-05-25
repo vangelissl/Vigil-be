d8004b4e232490751f03cc4371ce07afe225253f
Maria Stashenko <mahryia@gmail.com>
2026-05-25 22:37:05 +0300

    update current user dependency to use access token instead of refresh

--- src/vigil/security/dependencies.py
+++ src/vigil/security/dependencies.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, Cookie, HTTPException
+from fastapi import Depends, Header, HTTPException
+
+import jwt
 
 from typing import Annotated
 
@@ -12,18 +14,23 @@ from .hashing import PasswordHasher
 async def get_token_service() -> TokenService:
     return TokenService()
 
+
 async def get_current_user(
-        refresh_token: Annotated[str | None, Cookie()],
+        access_token: Annotated[str | None, Header()],
         service: TokenService = Depends(get_token_service)) -> CurrentUserDTO:
-	"""Returns the current user if they are logged, otherwise raises an exception"""
-	if not refresh_token:
-		raise HTTPException(status_code=401, detail="Not logged in")
-	user = service.decode_refresh_token(refresh_token)
-	
-	return CurrentUserDTO(
-		id=uuid.UUID(user.sub),
-		username=user.username
-	)
+    """Returns the current user if they are logged, otherwise raises an exception"""
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Not logged in")
+    try:
+        user = service.decode_token(access_token)
+    except:
+        raise HTTPException(status_code=401, detail="Not logged in")
+
+    return CurrentUserDTO(
+        id=uuid.UUID(user.sub),
+        username=user.username
+    )
+
 
 async def get_hasher():
-	return PasswordHasher()
+    return PasswordHasher()
