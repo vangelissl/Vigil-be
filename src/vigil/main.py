34397f93d0c9e1b98baafd48992eb2cdcad238e6
Maria Stashenko <mahryia@gmail.com>
2026-06-04 12:48:43 +0300

    update docker compose

--- src/vigil/main.py
+++ src/vigil/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from vigil_tasks.analysis import run_inference
 
 from vigil.core.config import settings
 
@@ -14,6 +13,8 @@ from vigil.modules.users.presentation.exception_handlers import register_excepti
 
 from vigil.modules.videos.presentation.api.v1.router import router as videos_router
 from vigil.modules.videos.presentation.exception_handlers import register_exception_handlers as register_videos_handlers
+
+from vigil.modules.analysis.presentation.api.v1.router import router as analyses_router
 
 def create_app() -> FastAPI:
     application = FastAPI(
@@ -39,15 +40,11 @@ def create_app() -> FastAPI:
     @application.get("/health")
     async def health_check() -> dict[str, str]:
         return {"status": "ok"}
-
-    @application.get("/test-task")
-    async def test_task():
-        run_inference.delay("test-123") # type: ignore
-        return {"status": "dispatched"}
     
     application.include_router(auth_router)
     application.include_router(users_router)
     application.include_router(videos_router)
+    application.include_router(analyses_router)
 
     return application
 
